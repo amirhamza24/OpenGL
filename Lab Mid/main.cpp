330076238e86dc7876b38951aaba206acf059940
Amir Hamza <52718336+amirhamza24@users.noreply.github.com>
2022-11-29 19:30:16 +0600

    Update main.cpp

--- Lab Mid/main.cpp
+++ Lab Mid/main.cpp
@@ -1,256 +1,263 @@
-#include<windows.h>
 #ifdef __APPLE__
 #include <GLUT/glut.h>
 #else
+#include<windows.h>
 #include <GL/glut.h>
 #endif
 #include <stdlib.h>
-#include<math.h>
-#define PI 3.1416
-#include <stdlib.h>
-#include <stdio.h>
+#include<stdio.h>
 
-void init()
-{
- glClearColor(2,1.9,1.9,1);
- glOrtho(-100,100,-100,100,-100,100);
-}
+double r;
+void reshape(int width, int height){
+  glViewport(0,0,width, height);
 
-void circle(float radiusX,float radiusY){
-    int i;
-    float angle=0.0;
-    glBegin(GL_LINE_LOOP);
-        for(i=0;i<100;i++){
-            angle= 2*PI*i/100;
-            glVertex2f(radiusX*cos(angle),radiusY*sin(angle));
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
 
-        }
-    glEnd();
+  gluPerspective(45.0, (double)width/(double)height, 1.0, 200.0);
 
 }
+void myDisplay(){
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-void line()
-{
- glClear(GL_COLOR_BUFFER_BIT);
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+  glTranslated(0,0,-20);
+  //glRotated(-45,1,1,0);
+  glRotated(r,0,1,0);
+  //glRotated(r,0,1,0);
 
+       ///door
+      glPushMatrix();
+       glColor3ub(102, 51, 0);
+       glBegin(GL_QUADS);
+        glVertex3d(-2,1.8,3.1);
+        glVertex3d(-2,-2,3.1);
+        glVertex3d(-0.8, -2,3.1);
+        glVertex3d(-0.8,1.8,3.1);
+       glEnd;
+      glPopMatrix();
 
+      ///front window1
+      glPushMatrix();
+       glColor3ub(102, 51, 0);
+       glBegin(GL_QUADS);
+        glVertex3d(-0.5,1.8,3.1);
+        glVertex3d(-0.5,0.5,3.1);
+        glVertex3d(0.6, 0.5,3.1);
+        glVertex3d(0.6,1.8,3.1);
+       glEnd;
+      glPopMatrix();
 
-glBegin(GL_QUADS);
-     glColor3f( 0.058 ,0.05,0.05 );//main road border
-     glColor3f(0.6,0.6,0.6);
+      ///front window2
+      glPushMatrix();
+       glColor3ub(102, 51, 0);
+       glBegin(GL_QUADS);
+        glVertex3d(1,1.8,3.1);
+        glVertex3d(1,0.5,3.1);
+        glVertex3d(2, 0.5,3.1);
+        glVertex3d(2,1.8,3.1);
+       glEnd;
+      glPopMatrix();
 
-        glVertex2d(-100,50);
-        glVertex2d(100,50);
-        glVertex2d(100,70);
-        glVertex2d(-100,70);
-glEnd();
+      ///front window3
+      glPushMatrix();
+       glColor3ub(102, 51, 0);
+       glBegin(GL_QUADS);
+        glVertex3d(2.4,1.8,3.1);
+        glVertex3d(2.4,0.5,3.1);
+        glVertex3d(3.4, 0.5,3.1);
+        glVertex3d(3.4,1.8,3.1);
+       glEnd;
+      glPopMatrix();
 
-glBegin(GL_QUADS);
-     glColor3f( 0.058 ,0.05,0.05 );//main road
-       glColor3f(0,0,0);
+      ///front window4
+      glPushMatrix();
+       glColor3ub(102, 51, 0);
+       glBegin(GL_QUADS);
+        glVertex3d(3.8,1.8,3.1);
+        glVertex3d(3.8,0.5,3.1);
+        glVertex3d(4.8, 0.5,3.1);
+        glVertex3d(4.8,1.8,3.1);
+       glEnd;
+      glPopMatrix();
 
-        glVertex2d(-100,50);
-        glVertex2d(100,50);
-        glVertex2d(100,-45);
-        glVertex2d(-100,-45);
-glEnd();
-glBegin(GL_QUADS);
-     glColor3f( 0.058 ,0.05,0.05 );//main road border
-       glColor3f(0.6,0.6,0.6);
+      ///back window1
+      glPushMatrix();
+       glColor3ub(102, 51, 0);
+       glBegin(GL_QUADS);
+        glVertex3d(-0.5,1.8,-3.1);
+        glVertex3d(-0.5,0.5,-3.1);
+        glVertex3d(0.6, 0.5,-3.1);
+        glVertex3d(0.6,1.8,-3.1);
+       glEnd;
+      glPopMatrix();
 
-        glVertex2d(-100,-45);
-        glVertex2d(100,-45);
-        glVertex2d(100,-65);
-        glVertex2d(-100,-65);
-glEnd();
+      ///back window2
+      glPushMatrix();
+       glColor3ub(102, 51, 0);
+       glBegin(GL_QUADS);
+        glVertex3d(1,1.8,-3.1);
+        glVertex3d(1,0.5,-3.1);
+        glVertex3d(2, 0.5,-3.1);
+        glVertex3d(2,1.8,-3.1);
+       glEnd;
+      glPopMatrix();
 
-glBegin(GL_QUADS);
-     glColor3f( 0.058 ,0.05,0.05 );//up strip
-       glColor3f(1,1,1);
+      ///back window3
+      glPushMatrix();
+       glColor3ub(102, 51, 0);
+       glBegin(GL_QUADS);
+        glVertex3d(2.4,1.8,-3.1);
+        glVertex3d(2.4,0.5,-3.1);
+        glVertex3d(3.4, 0.5,-3.1);
+        glVertex3d(3.4,1.8,-3.1);
+       glEnd;
+      glPopMatrix();
 
-        glVertex2d(-100,45);
-        glVertex2d(100,45);
-        glVertex2d(100,40);
-        glVertex2d(-100,40);
-glEnd();
-
-glBegin(GL_QUADS);
-     glColor3f( 0.058 ,0.05,0.05 );//down strip
-       glColor3f(1,1,1);
-
-        glVertex2d(-100,-40);
-        glVertex2d(100,-40);
-        glVertex2d(100,-35);
-        glVertex2d(-100,-35);
-glEnd();
-///////////////////////////////////////////////
-
-glBegin(GL_QUADS);
-     glColor3f( 0.058 ,0.05,0.05 );//middle strip1
-       glColor3f(1,1,1);
-
-        glVertex2d(-100,5);
-        glVertex2d(-80,5);
-        glVertex2d(-80,0);
-        glVertex2d(-100,0);
-glEnd();
-
-glPushMatrix();
-   glTranslatef(-60,2,0);
-   glColor3f(1,1,1);
-   circle(16,16);
-   glPopMatrix();
-
-glBegin(GL_QUADS);
-     glColor3f( 0.058 ,0.05,0.05 );//middle strip2
-      glColor3f(1,1,1);
-
-        glVertex2d(-70,5);
-        glVertex2d(-50,5);
-        glVertex2d(-50,0);
-        glVertex2d(-70,0);
-glEnd();
-
-glBegin(GL_QUADS);
-     glColor3f( 0.058 ,0.05,0.05 );//middle strip3
-       glColor3f(1,1,1);
-
-        glVertex2d(-20,5);
-        glVertex2d(-40,5);
-        glVertex2d(-40,0);
-        glVertex2d(-20,0);
-glEnd();
-
-
-glBegin(GL_QUADS);
-     glColor3f( 0.058 ,0.05,0.05 );//middle strip1.2
-       glColor3f(1,1,1);
-
-        glVertex2d(100,5);
-        glVertex2d(80,5);
-        glVertex2d(80,0);
-        glVertex2d(100,0);
-glEnd();
-glPushMatrix();
-   glTranslatef(60,2,0);
-   glColor3f(1,1,1);
-   circle(16,16);
-   glPopMatrix();
-
-glBegin(GL_QUADS);
-     glColor3f( 0.058 ,0.05,0.05 );//middle strip2.2
-       glColor3f(1,1,1);
-
-        glVertex2d(70,5);
-        glVertex2d(50,5);
-        glVertex2d(50,0);
-        glVertex2d(70,0);
-glEnd();
-
-glBegin(GL_QUADS);
-     glColor3f( 0.058 ,0.05,0.05 );//middle strip3.2
-      glColor3f(1,1,1);
-
-        glVertex2d(20,5);
-        glVertex2d(40,5);
-        glVertex2d(40,0);
-        glVertex2d(20,0);
-glEnd();
-/////////////////////////////////////
-
-glBegin(GL_QUADS);
-     glColor3f( 0.058 ,0.05,0.05 );//middle strip box
-       glColor3f(1,1,1);
-
-        glVertex2d(-5,5);
-        glVertex2d(10,5);
-        glVertex2d(10,0);
-        glVertex2d(-5,0);
-glEnd();
-
-glBegin(GL_QUADS);
-     glColor3f( 0.058 ,0.05,0.05 );//middle strip box
-      glColor3f(1,1,1);
-
-        glVertex2d(-5,10);
-        glVertex2d(10,10);
-        glVertex2d(10,15);
-        glVertex2d(-5,15);
-glEnd();
-
-glBegin(GL_QUADS);
-     glColor3f( 0.058 ,0.05,0.05 );//middle strip box
-       glColor3f(1,1,1);
-
-        glVertex2d(-5,20);
-        glVertex2d(10,20);
-        glVertex2d(10,25);
-        glVertex2d(-5,25);
-glEnd();
-
-glBegin(GL_QUADS);
-     glColor3f( 0.058 ,0.05,0.05 );//middle strip box
-       glColor3f(1,1,1);
-
-        glVertex2d(-5,35);
-        glVertex2d(10,35);
-        glVertex2d(10,30);
-        glVertex2d(-5,30);
-glEnd();
+      ///back window4
+      glPushMatrix();
+       glColor3ub(102, 51, 0);
+       glBegin(GL_QUADS);
+        glVertex3d(3.8,1.8,-3.1);
+        glVertex3d(3.8,0.5,-3.1);
+        glVertex3d(4.8, 0.5,-3.1);
+        glVertex3d(4.8,1.8,-3.1);
+       glEnd;
+      glPopMatrix();
 
 
+     ///front
+     glPushMatrix();
+      glColor3f(1.0f,1.0f,0.0f);
+      glBegin(GL_QUADS);
+       glVertex3f(6,3,3);
+       glVertex3f(-2,3,3);
+       glVertex3f(-6,-3,3);
+       glVertex3f(6,-3,3);
 
 
+      /*glPushMatrix();
+       glColor3ub(102, 51, 0);
+       glBegin(GL_QUADS);
+       glVertex3d(-2,1.8,3);
+       glVertex3d(-2,-2,3);
+       glVertex3d(-1, -2,3);
+       glVertex3d(-1,1.8,3);
+       glEnd;
+      glPopMatrix();
 
+      glPushMatrix();
+      glColor3ub(102, 51, 0);
+      glBegin(GL_QUADS);
+      glVertex3d(-0.5,1.8,3);
+      glVertex3d(-0.5,0,3);
+      glVertex3d(0.6, 0,3);
+      glVertex3d(0.6,1.8,3);
+      glEnd;
+      glPopMatrix();*/
+      glEnd();
+     glPopMatrix();
 
-glBegin(GL_QUADS);
-     glColor3f( 0.058 ,0.05,0.05 );//middle strip box
-       glColor3f(1,1,1);
+    ///back
+    glPushMatrix();
+     glColor3f(1.0f,1.0f,0.0f);
+     glBegin(GL_QUADS);
+      glVertex3f(6,3,-3);
+      glVertex3f(-2,3,-3);
+      glVertex3f(-6,-3,-3);
+      glVertex3f(6,-3,-3);
 
-        glVertex2d(-5,-5);
-        glVertex2d(10,-5);
-        glVertex2d(10,-10);
-        glVertex2d(-5,-10);
-glEnd();
+     glEnd();
+    glPopMatrix();
 
+    ///left side
+    glPushMatrix();
+     glColor3f(1.0f,0.0f,0.0f);
+     glBegin(GL_QUADS);
+      glVertex3f(-2,3,3);
+      glVertex3f(-2,3,-3);
+      glVertex3f(-6,-3,-3);
+      glVertex3f(-6,-3,3);
+     glEnd();
+    glPopMatrix();
 
+    ///right side
+    glPushMatrix();
+     glColor3f(1.0f,0.0f,0.0f);
+     glBegin(GL_QUADS);
+      glVertex3f(6,3,3);
+      glVertex3f(6,3,-3);
+      glVertex3f(6,-3,-3);
+      glVertex3f(6,-3,3);
+     glEnd();
+    glPopMatrix();
 
+    ///top side
+    glPushMatrix();
+     glColor3f(1.0f,1.0f,1.0f);
+     glBegin(GL_QUADS);
+      glVertex3f(-2,3,-3);
+      glVertex3f(6,3,-3);
+      glVertex3f(6,3,3);
+      glVertex3f(-2,3,3);
+     glEnd();
+    glPopMatrix();
 
-glBegin(GL_QUADS);
-     glColor3f( 0.058 ,0.05,0.05 );//middle strip box
-       glColor3f(1,1,1);
+  glutSwapBuffers();
 
-        glVertex2d(-5,-15);
-        glVertex2d(10,-15);
-        glVertex2d(10,-20);
-        glVertex2d(-5,-20);
-glEnd();
-
-glBegin(GL_QUADS);
-     glColor3f( 0.058 ,0.05,0.05 );//middle strip box
-       glColor3f(1,1,1);
-
-        glVertex2d(-5,-25);
-        glVertex2d(10,-25);
-        glVertex2d(10,-30);
-        glVertex2d(-5,-30);
-glEnd();
-
-
-
-
- glFlush();
 }
-int main()
+
+void rotates()
 {
-    printf("Name: Amir Hamza\n");
-    printf("Id: CSE 069 08020");
-    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-    glutInitWindowSize(800,700);
-    glutInitWindowPosition(100,100);
-    glutCreateWindow("Amir Hamza_CSE06908020");
-    init();
-    glutDisplayFunc(line);
+    r = r + 0.1;
+    glutPostRedisplay();
+}
+
+void rotates2()
+{
+
+    r = r+0.1;
+    glutPostRedisplay();
+}
+
+void keyMove(unsigned char key, int x, int y)
+{
+    switch(key)
+    {
+    case 'r':
+        glutIdleFunc(rotates);
+        break;
+
+    case 'a':
+        glutIdleFunc(rotates2);
+        break;
+
+    case 'b':
+        glutIdleFunc(NULL);
+        break;
+    }
+
+}
+
+
+int main(int argc, char *argv[])
+{
+
+    printf("Student Id: CSE 06908020\n");
+    printf("Name: Amir Hamza");
+    glutInit(&argc, argv);
+    glutInitWindowSize(1100,700);
+    //glutInitWindowPosition(10,10);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("ID: CSE06908020.. Name: Amir Hamza");
+    glEnable(GL_DEPTH_TEST);
+    glutReshapeFunc(reshape);
+    glutDisplayFunc(myDisplay);
+    glutKeyboardFunc(keyMove);
+
 
     glutMainLoop();
+
+    return EXIT_SUCCESS;
 }
